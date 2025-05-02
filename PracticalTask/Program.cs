2313b21e87003426809f1967e62acda3bb4789f1
MtimurM2907 <timur_mmm00@mail.ru>
2025-05-02 17:17:54 +0500

    Task4

--- PracticalTask/Program.cs
+++ PracticalTask/Program.cs
@@ -6,15 +6,18 @@
 
     static void Main(string[] args)
     {
-        text = Console.ReadLine();
         TextCheck();
+        StringActions();
+        RepeatCharacters();
+        FindingLargestSubstring();
     }
 
-    //проверка текста
+    //Проверка на корректный ввод
     public static void TextCheck()
     {
         try
         {
+            text = Console.ReadLine();
             bool[] textBools = new bool[text.Length];
             bool checkSymbol = false;
             for (int i = 0; i < text.Length; i++)
@@ -41,15 +44,15 @@
             {
                 throw new Exception($"Были введены не подходящие символы:{inappropriateCharacters}");
             }
-            StringActions();
         }
         catch (Exception ex)
         {
             Console.WriteLine("Произошла ошибка: " + ex.Message);
+            Environment.Exit(0);
         }
     }
 
-    //действия со строкой
+    //Действия со строкой
     public static void StringActions()
     {
         if (text.Length % 2 == 0)
@@ -72,10 +75,9 @@
             finalText += text;
         }
         Console.WriteLine(finalText);
-        RepeatCharacters();
     }
 
-    //повторение символов
+    //Подсчёт каждого символа в обработанной строке
     public static void RepeatCharacters()
     {
         var uniqueSymbol = new HashSet<char>();
@@ -101,6 +103,53 @@
             }
             Console.WriteLine($"Символ {uniqueSymbolList[i]} повторялся {symbolCount} раз(а)");
             symbolCount = 0;
+        }
+    }
+
+    //Нахождение наибольшей подстроки, которая начинается и заканчивается на гласную букву из «aeiouy»
+    public static void FindingLargestSubstring()
+    {
+        string vowelsLetters = "aeiouy";
+        bool findIndex = false;
+        int startIndex = 0;
+        for (int i = 0; i < finalText.Length; i++)
+        {
+            foreach (char j in vowelsLetters)
+            {
+                if (finalText[i] == j)
+                {
+                    startIndex = i;
+                    findIndex = true;
+                    break;
+                }
+            }
+            if (findIndex == true)
+            {
+                findIndex = false;
+                break;
+            }
+        }
+        int finishIndex = 0;
+        for (int i = finalText.Length - 1; i >= 0; i--)
+        {
+            foreach (char j in vowelsLetters)
+            {
+                if (finalText[i] == j)
+                {
+                    finishIndex = i;
+                    findIndex = true;
+                    break;
+                }
+            }
+            if (findIndex == true)
+            {
+                findIndex = false;
+                break;
+            }
+        }
+        for (int i = startIndex; i <= finishIndex; i++)
+        {
+            Console.Write($"{finalText[i]}");
         }
     }
 }
